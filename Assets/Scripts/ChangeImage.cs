fc6a87c8a578481c5eedbadc03f37b2d5034778f
YoungJoong-kim98 <rladudwnd012@gmail.com>
2025-01-21 20:33:29 +0900

    ChangeImage

---
+++ Assets/Scripts/ChangeImage.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightImage : MonoBehaviour
+{
+    public Image Image1;
+    public Image Image2;
+    public Image Image3;
+
+    public void RightButtonImage()
+    {
+ 
+        Sprite sprite1 = Resources.Load<Sprite>("Images/3");
+        Sprite sprite2 = Resources.Load<Sprite>("Images/4");
+        Sprite sprite3 = null;
+
+        if (Image1 != null && sprite1 != null) Image1.sprite = sprite1;
+        if (Image2 != null && sprite2 != null) Image2.sprite = sprite2;
+        if (Image3 != null) Image3.sprite = sprite3; 
+    }
+
+    public void LeftButtonImage()
+    {
+
+        Sprite sprite1 = Resources.Load<Sprite>("Images/0"); 
+        Sprite sprite2 = Resources.Load<Sprite>("Images/1"); 
+        Sprite sprite3 = Resources.Load<Sprite>("Images/2"); 
+
+        if (Image1 != null && sprite1 != null) Image1.sprite = sprite1;
+        if (Image2 != null && sprite2 != null) Image2.sprite = sprite2;
+        if (Image3 != null && sprite3 != null) Image3.sprite = sprite3; 
+    }
+}
